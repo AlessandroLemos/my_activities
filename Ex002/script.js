c480ac980959f99108c4bf0e3e34239dcc300b81
Alessandro Lemos <146662822+AlessandroLemos@users.noreply.github.com>
2023-10-17 19:35:05 -0400

    The second part of 10/17/23

--- Ex002/script.js
+++ Ex002/script.js
@@ -1,3 +1,18 @@
-function greeting() {
-    
+function wakeup() {
+    prompt("Who is going to enter today?")
+    let msg = document.getElementById('namesaid')
+    let thehead = document.getElementById('greeting')
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `Right now is ${hora} hours`
+
+    if (hora >= 0 && hora < 12) {
+        thehead.innerHTML = `Good Morning ${prompt.value}`
+    } if (hora >= 12 && hora < 18) {
+        thehead.innerHTML = `Good Afternoon ${prompt.value}`
+    } else {
+        thehead.innerHTML = `Good Evening ${prompt.value}`
+    }
 }
+
+
