05243b1e8dedc7110a3f4194c90053cebb8a3c9c
Alessandro Lemos <146662822+AlessandroLemos@users.noreply.github.com>
2023-10-21 19:16:47 -0400

    Completion of the activity

--- Ex002/script.js
+++ Ex002/script.js
@@ -1,18 +1,21 @@
 function wakeup() {
-    prompt("Who is going to enter today?")
+    let name = prompt("Who is going to enter today?", "Special one")
     let msg = document.getElementById('namesaid')
     let thehead = document.getElementById('greeting')
-    let data = new Date()
+    const data = new Date()
     let hora = data.getHours()
-    msg.innerHTML = `Right now is ${hora} hours`
-
+    let min = data.getMinutes()
+    let time = hora + ":" + min;
+    msg.innerHTML = `Right now, the time is ${time}`
+    
     if (hora >= 0 && hora < 12) {
-        thehead.innerHTML = `Good Morning ${prompt.value}`
+        thehead.innerHTML = `Good Morning ${name}`
+            document.body.style.background = '#D6B79B'
     } if (hora >= 12 && hora < 18) {
-        thehead.innerHTML = `Good Afternoon ${prompt.value}`
+        thehead.innerHTML = `Good Afternoon ${name}`
+            document.body.style.background = '#FB7E00'
     } else {
-        thehead.innerHTML = `Good Evening ${prompt.value}`
+        thehead.innerHTML = `Good Evening ${name}`
+            document.body.style.background ='#0454B3'
     }
 }
-
-
