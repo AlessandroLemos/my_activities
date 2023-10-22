5ad1320fe493c7a58f2f8c92268e0218367b4d5c
Alessandro Lemos <146662822+AlessandroLemos@users.noreply.github.com>
2023-10-21 21:31:40 -0400

    My third exercise
    A little note: It seems that every time you press the button with a value in it, it repeat the paragraph to the infinite. This will be addressed.

---
+++ Ex003/script.js
@@ -0,0 +1,21 @@
+function inform(){
+    let name = document.getElementById('namez')
+    let sal = document.getElementById('sal')
+    
+
+    if (name.value == 0 || sal.value == 0) {
+        alert("Please, insert your name and salary (It does not need to be real tough. Remember, this is just a test)")
+    } else {
+        let n1 = name.value
+        let vl = sal.value
+        let h2 = document.getElementById('h2')
+        let div = document.getElementById('sec__div')
+        const para = document.createElement("p")
+            para.innerHTML = ""
+            h2.innerHTML = `Your name is ${n1}`   
+            para.innerHTML = `And your salary is about ${vl}`
+            document.getElementById('sec__div').appendChild(para)
+
+            
+    }
+}
