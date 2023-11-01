bb5665489f8356f4c96d05ca0b87f31552d03819
Alessandro Lemos <146662822+AlessandroLemos@users.noreply.github.com>
2023-11-01 11:38:43 -0400

    Finishing this project
    Another time, there's the problem of repetition of inputs

--- Ex006/script.js
+++ Ex006/script.js
@@ -15,11 +15,15 @@ function media(){
                 alert("Não é possível ter uma nota negativa")
             } else {
                 result.innerHTML = `A média entre ${p1} e ${p2} é <strong>${ans}</strong>!`
-                if (ans > 10 && ans < 100) {
-                    if (ans < 55){
+                if (ans > 1.0 && ans < 10.0) {
+                    if (ans < 7.0){
                     var solut = document.createElement('h2')
-                    solut.innerHTML = `A média de corte é de 55, portanto você <br>NÃO passou.`
+                    solut.innerHTML = `A média de corte é de 7.0, portanto você <br>NÃO passou.`
                     document.getElementById("first__div").appendChild(solut)
+                    } else {
+                        var solut = document.createElement('h2')
+                        solut.innerHTML = `A média de corte é de 7.0, PARABÉNS <br>VOCÊ PASSOU.`
+                        document.getElementById("first__div").appendChild(solut)
                     }
                 }
             }
