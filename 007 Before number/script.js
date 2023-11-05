997f8038256c29da07b092768710184aee6a81a6
Alessandro Lemos <146662822+AlessandroLemos@users.noreply.github.com>
2023-11-04 21:59:02 -0400

    A small correction

--- 007 Before number/script.js
+++ 007 Before number/script.js
@@ -1,4 +1,5 @@
 let n1 = document.getElementById("num")
+const val = document.getElementById("result")
 
 function press(){
     if (n1.value == 0) {
@@ -7,8 +8,7 @@ function press(){
         let numb = Number(n1.value)
         let res = numb - 1
 
-        const para = document.createElement('h2')
-        para.innerHTML = `The number before ${numb} is: ${res}`
-        document.getElementById('first__div').appendChild(para)
+        val.innerHTML = ''
+        val.innerHTML = `The number before ${numb} is: ${res}`
     }
 }
