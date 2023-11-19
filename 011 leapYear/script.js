9f74f20db1b8602e6ef9cd6cd431e2862345ea4c
Alessandro Lemos <146662822+AlessandroLemos@users.noreply.github.com>
2023-11-18 22:48:59 -0400

    Little corrections

--- 011 leapYear/script.js
+++ 011 leapYear/script.js
@@ -6,7 +6,7 @@ const res = document.getElementById("res")
 function thisYear() {
     let today = document.getElementById("this year")
     today.innerHTML = ''
-        if ((now % 4== 0) && (now % 100 != 0) || (now % 400 == 0)) {
+        if ((now % 4 == 0) && (now % 100 != 0) || (now % 400 == 0)) {
             return today.innerHTML = `By the way, the year of <strong>${now}</strong> is a leap year`
         } else {
             return today.innerHTML = `By the way, the year of <strong>${now}</strong> is NOT a leap year`
@@ -18,7 +18,7 @@ function leapYear() {
         alert("Please, insert a year")
     } else {
         let y = Number(year.value)
-        if ((y % 4== 0) && (y % 100 != 0) || (y % 400 == 0)) {
+        if ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0)) {
             return res.innerHTML = `The year of ${y} is a leap year`
         } else {
             return res.innerHTML = `The year of ${y} is NOT a leap year`
